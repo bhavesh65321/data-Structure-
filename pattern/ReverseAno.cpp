8023a24062c4a096db12a2ceb9440154c2fcd57d
bhavesh <bhavesh65321@gmail.com>
2022-01-06 15:39:19 +0530

    reverseno

---
+++ pattern/ReverseAno.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int r=0;
+    int i=0;
+    int c=0;
+    while(n!=0)
+    {
+        r=r*10+(n%10);
+        n=n/10;
+    }
+    cout<<r;
+    return 0;
+}
