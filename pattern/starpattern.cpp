44aa6bb3b71ed1ceff891ea09447370c1d0e3bb0
bhavesh <bhavesh65321@gmail.com>
2022-01-06 13:36:34 +0530

    updated

--- pattern/starpattern.cpp
+++ pattern/starpattern.cpp
@@ -30,7 +30,7 @@ int main()
         }
          cout<<endl;
     }
-    for(int i=1;i<=n;i++)
+    /*for(int i=1;i<=n;i++)
     {
         for(int j=i-1;j>0;j--)
         {
@@ -44,7 +44,19 @@ int main()
             cout<<"* ";
         }
         cout<<endl;
+    }*/
+    for(int i=n;i>0;i--)  /*reverse of upper code*/
+    {
+        for(int j=n-i;j>0;j--)
+        {
+             cout<<"  ";
+        }
+        int s=(2*i-1);
+        for(int k=1;k<=s;k++)
+        {
+             cout<<"* ";
+        }
+         cout<<endl;
     }
-
     return 0;
 }
